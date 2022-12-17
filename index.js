bf4ec7b39b6228406646e8e0f962a29c065d3055
Иван Викторович <mollojb@bk.ru>
2022-12-17 16:52:19 +0300

    Update index.js

--- index.js
+++ index.js
@@ -8,15 +8,13 @@ import fs from 'fs'
 
 
 const token = 'токен вк'
-const allowedTypeAttachment = ['photo']
-
 const memesGroups = ['saintbeobanka', 'demotiva',] // список id групп
+const vk = new Vk(token, memesGroups)
 
 
 const botToken = 'ТокенБота'
 const telegram = new Telegram(botToken, '@IDканала')
 
-const vk = new Vk(token, memesGroups)
 
 const args = process.argv
 if(args[2] === 'get'){
@@ -26,17 +24,4 @@ if(args[2] === 'get'){
 if(args[2] === 'post'){
     telegram.postPartGroup(JSON.parse(fs.readFileSync('posts.txt', 'utf8')))
 }
-//const res = await vk.processAll()
-//vk.storePosts(res)
-
-//vk.storePosts(res)
-//await telegram.processVkPosts(res)
-// //let a = items.filter(val => val.attachments !== undefined)[0]
-// //console.log(a, a.attachments[0].photo.sizes.at(-1))
-//console.log(res, res.length)
-
-
-//await telegram.sendText('Test')
-//await telegram.sendPhoto('Test', p[0])
-//await telegram.sendMediaGroup('Test', p)
 console.log('-----------------------')
